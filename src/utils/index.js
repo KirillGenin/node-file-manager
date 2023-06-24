7f4f6cf1d7a81dc14f15bb60cddbd81d37738ebe
KirillGenin <332211op@gmail.com>
2023-06-24 23:15:02 +0300

    chore: add error message to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
 import { getUserName } from './getUserName.js';
 import { printcwd } from './printcwd.js';
 
-export { getUserName, printcwd };
+const INPUT_ERROR_MESSAGE = 'Invalid input';
+const OPERATION_ERROR_MESSAGE = 'Operation failed';
+
+export { INPUT_ERROR_MESSAGE, OPERATION_ERROR_MESSAGE, getUserName, printcwd };
